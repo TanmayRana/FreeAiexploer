80d014b54e738ac0809c5d0e0a7bde923daff3e4
Tanmay Rana <tanmayrana2001@gmail.com>
2025-07-11 07:36:57 +0530

    push all code 2nd

--- app/api/ai_model/route.ts
+++ app/api/ai_model/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // import { GetgenerateContent } from "@/lib/GetgenerateContent";
 
 // export async function GET(request: Request) {
